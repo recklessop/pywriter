d58f38bc7c570910b2575788d43f9d61963f3e5e
Justin Paul <justin@jpaul.me>
2020-04-20 15:47:38 -0400

    updates to insert

--- pywriter.py
+++ pywriter.py
@@ -2,11 +2,27 @@ import os
 import threading
 import time
 import pyodbc
+import random
+import urllib.request
 
 sql_server = os.environ.get('DBSERVER')
 sql_db = os.environ.get('DBDATABASE')
 sql_user = os.environ.get('DBUSER')
 sql_pass = os.environ.get('DBPASS')
+
+word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+response = urllib.request.urlopen(word_url)
+long_txt = response.read().decode()
+words = long_txt.splitlines()
+
+upper_words = [word for word in words if word[0].isupper()]
+name_words  = [word for word in upper_words if not word.isupper()]
+one_name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
+
+
+def rand_name():
+   name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
+   return name
 
 def sql_connect():
     global sqlconn
@@ -14,7 +30,7 @@ def sql_connect():
     sqlconn = pyodbc.connect('driver={ODBC Driver 17 for SQL Server};server=%s;database=%s;uid=%s;pwd=%s' %
         ( sql_server, sql_db, sql_user, sql_pass ) )
 
-def run_writer():
+def run_writer(name, age, city):
     print("Function Running...")
 
     global sqlconn
@@ -30,13 +46,12 @@ def run_writer():
         results.append(dict(zip(columns, row)))
 
     print(results)
-    
-    #cursor.execute('''
-    #            INSERT INTO TestDB.dbo.Person (Name, Age, City)
-    #            VALUES
-    #            ('Bob',55,'Montreal'),
-    #            ('Jenny',66,'Boston')
-    #            ''')
+
+    cursor.execute('''
+                INSERT INTO TestDB.dbo.Person (Name, Age, City)
+                VALUES
+                (%s,%s,%s)
+                ''') % (name, str(age), city)
     sqlconn.commit()
 
 
@@ -54,7 +69,12 @@ sql_connect()
 
 var = 1
 while var == 1 :
+
+    age = random.randint(1,101)
+    name = rand_name()
+    city = rand_name()
+
     start = time.time()
-    run_writer()
+    run_writer(name, age, city)
     end = time.time()
     print("Function took: ", (end -start))
