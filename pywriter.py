d92885330f2478b31bef9320fe876ee29142fded
Justin Paul <justin@jpaul.me>
2020-04-20 20:51:28 -0400

    add influxdb tracking

--- pywriter.py
+++ pywriter.py
@@ -5,6 +5,7 @@ import pyodbc
 import random
 import urllib.request
 import string
+from influxdb import InfluxDBClient
 
 sql_server = os.environ.get('DBSERVER')
 sql_db = os.environ.get('DBDATABASE')
@@ -12,6 +13,7 @@ sql_user = os.environ.get('DBUSER')
 sql_pass = os.environ.get('DBPASS')
 iterations = os.environ.get('ITERATIONS')
 junksize = os.environ.get('JUNKSIZE')
+hostname = os.environ.get('HOSTNAME')
 
 word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
 response = urllib.request.urlopen(word_url)
@@ -24,6 +26,13 @@ name_words  = [word for word in upper_words if not word.isupper()]
 def rand_name():
    name = ' '.join([name_words[random.randint(0, (len(name_words) - 1))] for i in range(2)])
    return name
+
+def influx_connect():
+    global influxconn
+    print("Connecting to InfluxDB...")
+    influxclient = InfluxDBClient(host='influxdb', port=8086)
+    influxclient.switch_database('sqldata')
+
 
 def sql_connect():
     global sqlconn
@@ -68,12 +77,29 @@ print("Iterations per loop: ", iterations)
 print("Junksize (KiloBytes): ", int(junksize))
 
 global sqlconn
+global influxconn
 
 sql_connect()
+influx_connect()
 
 var = 1
 while var == 1 :
+    data = []
     start = time.time()
     run_writer(iterations, int(junksize))
     end = time.time()
+    elapsed = (end - start)
     print("Function took: ", (end - start))
+
+    json_body = [
+    {
+        "measurement": "writelatency",
+        "tags": {
+            "writer": "{hostname}"
+        },
+        "time": "{start}",
+        "fields": {
+            "duration": {elapsed}
+        }
+    }]
+    influxclient.write_points(json_body, database='sqldata', time_precision='ms', batch_size=10000, protocol='json')
